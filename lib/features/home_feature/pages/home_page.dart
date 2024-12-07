83611d29067287d80f989f83ff92122ee332d249
sherif <sherif_shamel15@yahoo.com>
2024-12-07 23:05:40 +0200

    moved the cubit to the addPlayerFeature

--- lib/features/home_feature/pages/home_page.dart
+++ lib/features/home_feature/pages/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mal3abna/core/config/page_route_names.dart';
 import 'package:mal3abna/data/players_data.dart';
-import 'package:mal3abna/features/home_feature/cubits/add_players_cubit.dart';
 import 'package:mal3abna/features/home_feature/widgets/home_player_widget.dart';
 import 'package:mal3abna/main.dart';
 
@@ -11,35 +9,28 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AddPlayersCubit(),
-        )
-      ],
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Available Players",
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {
-                navigatorKey.currentState!.pushNamed(PagesRouteName.addPlayerView);
-              },
-              icon: const Icon(Icons.add),
-            ),
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Available Players",
         ),
-        body: GridView.builder(
-          itemCount: playersList.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2),
-          itemBuilder: (context, index) => GamePlayerWidget(
-            index: index,
-            playerModel: playersList[index],
-            isSelected: playersList[index].isSelected,
+        actions: [
+          IconButton(
+            onPressed: () {
+              navigatorKey.currentState!.pushNamed(PagesRouteName.addPlayerView);
+            },
+            icon: const Icon(Icons.add),
           ),
+        ],
+      ),
+      body: GridView.builder(
+        itemCount: playersList.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2),
+        itemBuilder: (context, index) => GamePlayerWidget(
+          index: index,
+          playerModel: playersList[index],
+          isSelected: playersList[index].isSelected,
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Mal3abna App",
       theme: ApplicationThemeManager.lightTheme,
       initialRoute: PagesRouteName.initial,
