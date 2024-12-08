bbdab12a34a6b64ace573c5cda2aa17a6c93fb85
sherif <sherif_shamel15@yahoo.com>
2024-12-08 12:03:05 +0200

    AddPlayerCubit in AddPlayerView

--- lib/features/add_player_feature/pages/add_player_view.dart
+++ lib/features/add_player_feature/pages/add_player_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mal3abna/features/add_player_feature/cubits/add_players_cubit.dart';
 import 'package:mal3abna/features/add_player_feature/cubits/add_players_states.dart';
 import 'package:mal3abna/features/add_player_feature/widgets/add_player_form.dart';
+import 'package:mal3abna/main.dart';
 
 class AddPlayerView extends StatelessWidget {
   const AddPlayerView({super.key});
@@ -14,15 +15,26 @@ class AddPlayerView extends StatelessWidget {
           title: const Text("Add Player"),
           centerTitle: false,
         ),
-        body: const AddPlayerForm()
+        body: BlocConsumer<AddPlayersCubit, AddPlayersStates>(
+          builder: (context, state) => const AddPlayerForm(),
+          listener: (BuildContext context, state) {
+            switch (state) {
+              case AddPlayerLoading():
+                {
+                  const CircularProgressIndicator();
+                }
 
-        //TODO:
-        // BlocConsumer<AddPlayersCubit, AddPlayersStates>(
-        //   listener: (context, state) {
-        //
-        //   },
-        //   builder: (context, state) => const AddPlayerForm(),
-        // ),
-        );
+              case AddPlayerFailure():
+                {
+                  Center(child: Text(state.errorMsg));
+                }
+
+              case AddPlayerSuccess():
+                {
+                  navigatorKey.currentState!.pop();
+                }
+            }
+          },
+        ));
   }
 }
--- lib/features/home_feature/widgets/home_player_widget.dart
+++ lib/features/home_feature/widgets/home_player_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mal3abna/data/players_data.dart';
-import 'package:mal3abna/generated/assets.dart';
 import 'package:mal3abna/models/player_model.dart';
 
 class GamePlayerWidget extends StatefulWidget {
